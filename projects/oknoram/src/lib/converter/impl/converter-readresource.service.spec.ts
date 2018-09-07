00f724bb851e2bfeb62443033d9d3ecc9cbcd6aa
gfoo <gilles.faucherand@gmail.com>
2018-09-07 12:37:30 +0200

    converter test

--- projects/oknoram/src/lib/converter/impl/converter-readresource.service.spec.ts
+++ projects/oknoram/src/lib/converter/impl/converter-readresource.service.spec.ts
@@ -1,44 +1,86 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import { ReadResource, ReadTextValueAsString } from '@knora/core';
+import { PropertyDef } from 'oknoram/lib/mapping/property-def';
+import { PropertyType } from '../../mapping/property-type';
+import { ResourceMapping } from '../../mapping/resource-mapping';
 import { OknoramConfig, OknoramConfigToken } from '../../oknoram-config';
 import { ConverterReadresourceService } from './converter-readresource.service';
 
-describe('ConverterReadresourceService', () => {
-  let oknoramConfigSpy: jasmine.SpyObj<OknoramConfig>;
+export class TestModel {
+  iriValue: string;
+  labelValue: string;
+  strValue: string;
+}
 
+export class OknoramConfigStub implements OknoramConfig {
+  knoraApiBaseUrl = 'knoraApiBaseUrl';
+}
+
+describe('ConverterReadresourceService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        ConverterReadresourceService,
+        {
+          provide: ConverterReadresourceService,
+          useClass: ConverterReadresourceService
+        },
         {
           provide: OknoramConfigToken,
-          useValue: jasmine.createSpyObj('OknoramConfigToken', [
-            'knoraApiBaseUrl'
-          ])
+          useClass: OknoramConfigStub
         }
       ]
     });
-    oknoramConfigSpy = TestBed.get(OknoramConfigToken);
   });
 
-  // it('should be created', inject(
-  //   [ConverterReadresourceService],
-  //   (service: ConverterReadresourceService) => {
-  //     expect(service).toBeTruthy();
-  //   }
-  // ));
+  it('should be created', inject(
+    [ConverterReadresourceService],
+    (service: ConverterReadresourceService) => {
+      expect(service).toBeTruthy();
+      expect(service instanceof ConverterReadresourceService).toBeTruthy();
+    }
+  ));
 
-  // it('#knoraApiBaseUrl should return stubbed value from a spy', () => {
-  //   const stubValue = 'stub value';
-  //   oknoramConfigSpy.knoraApiBaseUrl.and.returnValue(stubValue);
+  it('OknoramConfig#knoraApiBaseUrl should return stubbed value from a spy', inject(
+    [OknoramConfigToken],
+    (oknoramConfig: OknoramConfig) => {
+      expect(oknoramConfig).toBeTruthy();
+      expect(oknoramConfig.knoraApiBaseUrl).toBe('knoraApiBaseUrl');
+    }
+  ));
 
-  //   // expect(masterService.getValue())
-  //   //   .toBe(stubValue, 'service returned stub value');
-  //   expect(oknoramConfigSpy.knoraApiBaseUrl.calls.count()).toBe(
-  //     1,
-  //     'spy method was called once'
-  //   );
-  //   expect(
-  //     oknoramConfigSpy.knoraApiBaseUrl.calls.mostRecent().returnValue
-  //   ).toBe(stubValue);
-  // });
+  it('should convert', inject(
+    [ConverterReadresourceService, OknoramConfigToken],
+    (service: ConverterReadresourceService, oknoramConfig: OknoramConfig) => {
+      const rm = new ResourceMapping(TestModel);
+      rm.iri = 'iriValue';
+      rm.label = 'labelValue';
+      rm.propertyMapping('strValue', {
+        type: PropertyType.TextValue,
+        name: null,
+        optional: false
+      } as PropertyDef);
+
+      spyOn(service, 'getAttFullName').and.returnValue('varName');
+
+      const propValue = new ReadTextValueAsString(null, null, 'value');
+      const res = {
+        id: 'iri',
+        label: 'label',
+        properties: {
+          varName: [propValue]
+        },
+        type: null,
+        incomingRegions: null,
+        incomingStillImageRepresentations: null,
+        incomingLinks: null,
+        stillImageRepresentationsToDisplay: null
+      } as ReadResource;
+
+      const converted = service.convert<TestModel>(rm, res);
+      expect(converted).toBeTruthy();
+      expect(converted.iriValue).toBe(res.id);
+      expect(converted.labelValue).toBe(res.label);
+      expect(converted.strValue).toBe(propValue.getContent());
+    }
+  ));
 });
--- projects/oknoram/src/lib/converter/impl/converter-readresource.service.ts
+++ projects/oknoram/src/lib/converter/impl/converter-readresource.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 import { ReadResource } from '@knora/core';
+import { PropertyDef } from '../../mapping/property-def';
 import { ResourceMapping } from '../../mapping/resource-mapping';
 import { OknoramConfig, OknoramConfigToken } from '../../oknoram-config';
 import { ConverterService } from '../converter.service';
@@ -10,6 +11,13 @@ import { ConverterService } from '../converter.service';
 export class ConverterReadresourceService implements ConverterService {
   constructor(@Inject(OknoramConfigToken) private config: OknoramConfig) {}
 
+  getAttFullName(rm: ResourceMapping, att: PropertyDef): string {
+    // FIXME: really depends on JSONLD rules...
+    return `${this.config.knoraApiBaseUrl}/ontology/${rm.def.projectCode}/${
+      rm.def.projectShortname
+    }/v2#${att.name}`;
+  }
+
   convert<T>(rm: ResourceMapping, res: ReadResource): T {
     const obj = {};
     obj[rm.iri] = res.id;
@@ -17,12 +25,10 @@ export class ConverterReadresourceService implements ConverterService {
       obj[rm.label] = res.label;
     }
     rm.attributes.forEach((att, key) => {
-      const fullAttName = `${this.config.knoraApiBaseUrl}/ontology/${
-        rm.def.projectCode
-      }/${rm.def.projectShortname}/v2#${att.name}`;
-      if (!(fullAttName in res.properties) && !att.optional) {
+      const fullAttName = this.getAttFullName(rm, att);
+      if (!(res.properties && fullAttName in res.properties) && !att.optional) {
         throw new Error(`Cannot find property ${fullAttName} in query result`);
-      } else if (fullAttName in res.properties) {
+      } else if (res.properties && fullAttName in res.properties) {
         obj[key] = res.properties[fullAttName][0].getContent();
       } else {
         obj[key] = null;
