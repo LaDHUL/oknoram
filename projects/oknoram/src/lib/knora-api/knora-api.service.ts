c660e8251a7c1816396be8de79c01707057e0c36
gfoo <gilles.faucherand@gmail.com>
2018-09-10 22:49:52 +0200

    only knora service knows the json content

--- projects/oknoram/src/lib/knora-api/knora-api.service.ts
+++ projects/oknoram/src/lib/knora-api/knora-api.service.ts
@@ -1,9 +1,11 @@
 import { Observable } from 'rxjs';
+import { ResourceMapping } from '../mapping/resource-mapping';
 import { KnoraResource } from './knora-resource';
 
 export abstract class KnoraApiService {
   abstract executeQuery(
     query: string,
+    rm: ResourceMapping,
     pageIndex?: number
   ): Observable<KnoraResource[]>;
   abstract countQuery(query: string): Observable<number>;
